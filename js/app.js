90eb5a9d6db85252b088baf02d6e34f3b1e6ef40
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-03-11 13:15:18 +0600

    4th bug (reported post) solved

--- js/app.js
+++ js/app.js
@@ -150,10 +150,11 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
+  
 };
 
 const loadPosts = async () => {
